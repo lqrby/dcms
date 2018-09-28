4364f4003173c8169569a25ada990e355043f372
lqrby <748862180@qq.com>
2018-09-28 10:06:20 +0800

    通用环境通用流程

--- ChengGuan/common/appReportPicture.py
+++ ChengGuan/common/appReportPicture.py
@@ -4,12 +4,13 @@ from config.Log import logging
 
 # 移动端案卷上报图片
 def test_app_ReportPicture(imgUrl,picturePath):
-    files = {'upload': ('image', open(picturePath,'rb'),'multipart/form-data')}
-    objsbres = requests.post(url=imgUrl,files=files).text
-    obj_result = re.compile("<issuc>(.*?)</issuc>").search(objsbres).group()
-    if obj_result:
-        print("案卷图片上传成功")
-    else:
-        print("XXXXXXXXXX执法局:案卷图片出现问题XXXXXXXXXX")
+    for i,img in enumerate(picturePath):
+        files = {'upload': ('image', open(img,'rb'),'multipart/form-data')}
+        objsbres = requests.post(url=imgUrl,files=files).text
+        obj_result = re.compile("<issuc>(.*?)</issuc>").search(objsbres).group()
+        if obj_result:
+            print("图片%d上传成功"%i)
+        else:
+            print("XXXXXXXXXX图片%d出现问题XXXXXXXXXX"%i+1)
 
     
