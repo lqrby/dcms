1b06890c0802e96623ac577a79450a3c073fa61e
lqrby <748862180@qq.com>
2019-01-17 15:27:13 +0800

    提交上传兴趣点图标脚本及综合查询脚本

--- ChengGuan/common/appReportPicture.py
+++ ChengGuan/common/appReportPicture.py
@@ -1,4 +1,4 @@
-import requests,re
+import requests,re,time
 from common.constant_all import getConstant
 from config.Log import logging
 
@@ -6,14 +6,12 @@ from config.Log import logging
 def test_app_ReportPicture(imgUrl,picturePath):
     for i,img in enumerate(picturePath):
         files = {'upload': ('image', open(img,'rb'),'multipart/form-data')}
-        objsbres = requests.post(url=imgUrl,files=files).text
-        if '<issuc>' in objsbres:
-            obj_result = re.compile("<issuc>(.*?)</issuc>").search(objsbres).group()
+        objsbres = requests.post(url=imgUrl,files=files,timeout = 15)
+        objsbres.connection.close()
+        time.sleep(1)
+        if 'true' in objsbres.text:
+            print("图片{0}上传成功".format(i+1))
         else:
-            obj_result = False
-        if obj_result:
-            print("图片%d上传成功"%i)
-        else:
-            print("XXXXXXXXXX图片%d出现问题XXXXXXXXXX"%i+1)
+            print("XXXXXXXXXX图片{0}出现问题XXXXXXXXXX".format(i+1))
 
     
